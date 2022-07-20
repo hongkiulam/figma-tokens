8ffe3757aef64d97d61219899c43a0608ecb09da
Haydon Lam <lamhaydon@gmail.com>
2022-07-20 22:05:52 +0100

    Update cache invalidation script

--- utils/purgeDist.js
+++ utils/purgeDist.js
@@ -1,20 +1,37 @@
+// @ts-check
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
 
-const distPath = path.join(__dirname, "../dist");
-const distFiles = fs.readdirSync(distPath);
-
-for (const fileName of distFiles) {
+const invalidate = (path) => {
   const options = {
     hostname: "purge.jsdelivr.net",
     port: 443,
-    path: `/gh/hongkiulam/figma-tokens@latest/${fileName}`,
+    path: `/gh/hongkiulam/figma-tokens@latest/${path}`,
     method: "GET",
   };
   https
     .request(options, (res) => {
-      console.log(`Purged cache for ${options.path} with status ${res.statusCode}`);
+      console.log(
+        `Purged cache for ${options.path} with status ${res.statusCode}`
+      );
     })
     .end();
-}
+};
+
+const rootDir = path.join(__dirname, "../");
+const invalidateFilesInDir = (relativeDirPath) => {
+  const filesAndFolders = fs.readdirSync(path.join(rootDir, relativeDirPath));
+  for (const fileOrFolderName of filesAndFolders) {
+    const relativeFileOrFolderPath = `${relativeDirPath}/${fileOrFolderName}`;
+    const isDir = fs
+      .lstatSync(path.join(rootDir, relativeFileOrFolderPath))
+      .isDirectory();
+    if (isDir) {
+      invalidateFilesInDir(relativeFileOrFolderPath);
+    } else {
+      invalidate(relativeFileOrFolderPath)
+    }
+  }
+};
+invalidateFilesInDir("dist");
