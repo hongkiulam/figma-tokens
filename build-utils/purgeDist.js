12ea263ea1ca64ae97bc167d635b10eda397385a
Haydon Lam <lamhaydon@gmail.com>
2023-02-05 18:19:54 +0000

    Allow purging of specific branch

--- build-utils/purgeDist.js
+++ build-utils/purgeDist.js
@@ -3,11 +3,17 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 
+console.log("Allowed options: --branch=<branch-to-purge>");
+const branch =
+  process.argv
+    .find((arg) => arg.includes("--branch="))
+    ?.replace("--branch=", "") || "latest";
+
 const invalidate = (path) => {
   const options = {
     hostname: "purge.jsdelivr.net",
     port: 443,
-    path: `/gh/hongkiulam/figma-tokens@latest/${path}`,
+    path: `/gh/hongkiulam/figma-tokens@${branch}/${path}`,
     method: "GET",
   };
   https
@@ -30,7 +36,7 @@ const invalidateFilesInDir = (relativeDirPath) => {
     if (isDir) {
       invalidateFilesInDir(relativeFileOrFolderPath);
     } else {
-      invalidate(relativeFileOrFolderPath)
+      invalidate(relativeFileOrFolderPath);
     }
   }
 };
